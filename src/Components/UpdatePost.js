fa5237854b3b69c1309524616292164264badd95
skumarscse-skillsafari <sathish.kumar@skillsafari.in>
2024-04-01 21:14:28 +0530

    blog-app

--- src/Components/UpdatePost.js
+++ src/Components/UpdatePost.js
@@ -19,7 +19,7 @@ const UpdatePost = () => {
       .get(`${BASE_URL}/posts/${id}`)
       .then((res) => setPost(res?.data?.data))
       .catch((error) => alert(error.response.data.message));
-  });
+  }, []);
   const handleChange = (e) => {
     const { name, value } = e.target;
     setPost((prev) => {
